e886b207a5e75a6c620d43227fc0155c7cad2b3c
NoBey <nobeycn@qq.com>
2017-06-22 21:21:02 +0800

    add easy model

--- README.md
+++ README.md
@@ -10,5 +10,15 @@
 ----
 ### show
 - `bool`  是否显示input 文字 默认显示
+---
 ### style
-- `object` 修改css 
+- `object` 修改css
+---
+### inputStyle
+- `object` 内部 input css
+---
+### isBlod
+- `bool`  使用blob创建url图片  (未完成)
+---
+### easy
+- `bool`  使用简单模式 适合用于头像上传
--- src/main.js
+++ src/main.js
@@ -1,30 +1,40 @@
 const React = require('react')
 
 class ReadImg extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      easyShow: false,
+      easyImg: ''
+    }
+  }
   onchange(){
     const cb = this.props.result
-    const isBlod = this.props.isBlod
+    const { isBlod, easy} = this.props
+    const that = this
     const file = this.refs.id.files[0]
     if(!/image\/\w+/.test(file.type))return cb(false);
     let reader = new FileReader()
-    reader.readAsDataURL(file);
+    reader.readAsDataURL(file)
     reader.onload=function(e){
       let result = this.result
+      if(easy) that.setState({easyImg: result, easyShow: true})
       if(isBlod) result = toUrl(result)
-      return cb(this.result)
+      return cb(result)
     }
   }
-
+  // 测试中未完成
   toUrl(result){
     return result
   }
   render() {
-    const {show, style, inputStyle} = this.props
+    const {show, style, inputStyle, easy} = this.props
+    const { easyShow, easyImg } = this.state
     const showcss = show ? {} : {opacity: 0}
-
     return (
       <div style={ style }>
-        <input style={{ ...showcss, ...inputStyle}} ref="id" type="file" onChange={this.onchange.bind(this)} />
+        { easyShow ? '' : <input style={{ ...showcss, ...inputStyle}} ref="id" type="file" onChange={this.onchange.bind(this)} /> }
+        { (easy && easyShow) ?  <img style={{width: '100%', height: '100%'}} src={easyImg} onClick={()=>this.setState({easyShow: false})} /> : '' }
       </div>
     );
   }
@@ -35,7 +45,8 @@ ReadImg.defaultProps = {
   result: a => console.log(a),
   style : {},
   isBlod: false,
-  inputStyle: {width: '100%', height: '100%'}
+  inputStyle: {width: '100%', height: '100%'},
+  easy: false
 }
 
 
