facbfe801226fd166b4798b0755e27604fb80110
NoBey <786964300@qq.com>
2017-08-20 01:58:49 +0800

    fix id

--- src/main.js
+++ src/main.js
@@ -10,9 +10,9 @@ class ReadImg extends React.Component {
   }
   onchange(){
     const cb = this.props.result
-    const { isBlod, easy} = this.props
+    const { isBlod, easy, id} = this.props
     const that = this
-    const file = document.getElementById('_ReactReadImg').files[0]
+    const file = document.getElementById( id ? id : '_ReactReadImg').files[0]
     if(!/image\/\w+/.test(file.type))return cb(false);
     let reader = new FileReader()
     reader.readAsDataURL(file)
@@ -28,12 +28,12 @@ class ReadImg extends React.Component {
     return result
   }
   render() {
-    const {show, style, inputStyle, easy} = this.props
+    const {show, style, inputStyle, easy, id} = this.props
     const { easyShow, easyImg } = this.state
     const showcss = show ? {} : {opacity: 0}
     return (
       <div style={ style }>
-        { easyShow ? '' : <input style={{ ...showcss, ...inputStyle}} id='_ReactReadImg' type="file" onChange={this.onchange.bind(this)}  accept="image/jpg,image/jpeg,image/png,image/gif" /> }
+        { easyShow ? '' : <input style={{ ...showcss, ...inputStyle}} id={ id ? id : '_ReactReadImg' } type="file" onChange={this.onchange.bind(this)}  accept="image/jpg,image/jpeg,image/png,image/gif" /> }
         { (easy && easyShow) ?  <img style={{width: '100%', height: '100%'}} src={easyImg} onClick={()=>this.setState({easyShow: false})} /> : '' }
       </div>
     );
